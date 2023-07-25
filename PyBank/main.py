07e6c2dff00d7b7ed86743585b264e97015def45
Barbsbhg <Barbsbhg@gmail.com>
2023-07-24 22:08:00 -0700

    saving progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,28 @@
+#read file
 import os
 import csv
 
+budget_data = os.path. join('PyBank', 'Resources', 'budget_data.csv') 
 
-bankcsv = os.path.join("PyBank", "Resources", "budget_data.csv")
-
-with open(bankcsv) as csvfile:
+with open(budget_data) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
     # skip the header row
     header = next(csvfile)
-    print(f"header: {header}")
     
+    #find total number of months included in the dataset
+    #empty list to hold row 0 (months) count
     rowcount = []
 
+    #read through each row and count the months
     for row in csv_reader:
         rowcount.append(row[0])
         totalnumbermonths = len(rowcount)
     
-    print(str(totalnumbermonths))
+    print(f" Total number of months {totalnumbermonths}")
+
+    nettotal = []
+
+    for row in csv_reader:
+        nettotal.append int(row[1])  
+    print(f" Net total is {nettotal}")
